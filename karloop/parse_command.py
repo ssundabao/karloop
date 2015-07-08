b030d32deb8d6d0e61010721ad70b6117637cba8
lizhihao <karlvorndoenitz@gmail.com>
2015-07-08 16:31:01 +0800

    add parse command file

---
+++ karloop/parse_command.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+__author__ = 'lizhihao'
+
+
+import sys
+
+
+def parse_command_line(application, default):
+    args = sys.argv
+    for arg in args:
+        if "--port=" in arg:
+            try:
+                default = arg.split("=", 1)
+            except Exception, e:
+                print e
+                print "listen port error"
+                exit()
+    application.listen(default)
